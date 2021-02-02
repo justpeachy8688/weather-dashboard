c246eb4898a03337f6ad1832f0359f7022aa6949
Taylor Sanders <tsanders3515@gmail.com>
2021-02-01 17:46:49 -0900

    current weather data

--- assets/script.js
+++ assets/script.js
@@ -16,25 +16,8 @@ $("#search-button").on("click", function (event) {
     //console.log(event.target);
     var city = $("#search-value").val()
     console.log(city)
-    $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&apikey=25d8d087027fd4ca304fa1a83a21cf96",
-        method: 'GET',
-    }).then(function (response) {
-        console.log(response)
-        addCityToHistory(response.name)
-        renderHistoryItem(response.name)
-        var temperature = response.main.temp
-        console.log(temperature)
-        var windSpeed = response.wind.speed
-        console.log(windSpeed)
-        var humidity = response.main.humidity
-        console.log(humidity)
-        //focus on last- where do i find it?
-        //var uvIndex = 
-        todayWeather(temperature, windSpeed, humidity);
-    }).catch(function (error) {
-        console.log(error)
-    })
+    getcurrentWeather(city);
+
 })
 //2. Save to local Storage
 function addCityToHistory(city) {
@@ -54,25 +37,71 @@ function renderHistoryItem(city) {
     document.getElementById("search-history").prepend(li)
 }
 //Write HTML - Use JavaScript to write the HTML in an empty div by creating the element and adding bootstrap classes to it.
-//var tempEl
+/*THEN I am presented with current and future conditions for that city and that city is added to the search history
+1. Fetch to get weather data from that city using OpenWeatherAPI
+2. put the data from that fetch into my HTML*/
 
-function todayWeather(temperature, windSpeed, humidity) {
-    // var cityEl = document.getElementById('forecast-header').innerHTML = city.val;
+//conditional statement that checks the value of the UVI and compares it with the set value that 
+//0-3 green background, greater than 3 but less than 7 warning orange, 7+ red
+//use a button with built in features danger etc
+//var date = new Date(response.daily[i].dt).toLocalDateString()
+
+function todayWeather(temperature, windSpeed, humidity, name) {
     var tempEl = document.getElementById('temperature')
     tempEl.textContent = temperature
     var humidEl = document.getElementById('humidity')
-    humidEl.textContent = humidity
+    humidEl.textContent = "Humidity: " + humidity
     var windEl = document.getElementById('wind-speed')
     windEl.textContent = windSpeed
+    var nameEl = document.getElementById('forecast-header')
+    nameEl.textContent = name
 }
 
-//Basic CSS
 
-/*THEN I am presented with current and future conditions for that city and that city is added to the search history
-1. Fetch to get weather data from that city using OpenWeatherAPI
-2. put the data from that fetch into my HTML using $(html element).text("TEXT")
 
-WHEN I view current weather conditions for that city
+function getcurrentWeather(searchQuery) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=imperial&apikey=25d8d087027fd4ca304fa1a83a21cf96",
+        method: 'GET',
+    }).then(function (response) {
+        console.log(response)
+        addCityToHistory(response.name)
+        renderHistoryItem(response.name)
+        var name = response.name
+        console.log(name)
+        var temperature = response.main.temp
+        console.log(temperature)
+        var windSpeed = response.wind.speed
+        console.log(windSpeed)
+        var humidity = response.main.humidity
+        console.log(humidity)
+        //focus on last- where do i find it?
+
+        todayWeather(temperature, windSpeed, humidity, name);
+        getWeatherData(response.coord.lat, response.coord.lon)
+    }).catch(function (error) {
+        console.log(error)
+    })
+
+}
+
+function getWeatherData(lat, lon) {
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=25d8d087027fd4ca304fa1a83a21cf96",
+        method: 'GET',
+    }).then(function (response) {
+        console.log(response)
+        console.log("uviIndex", response.current.uvi)
+        var uviIndex = response.current.uvi
+        console.log(uviIndex)
+    })
+}
+
+function todayUvi(uviIndex) {
+    var uviEl = document.getElementById('uv-index')
+    uviEl.textContent = uviIndex
+}
+/*WHEN I view current weather conditions for that city
 THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 1. Pull different places of information from the API
 
@@ -81,10 +110,10 @@ THEN I am presented with a color that indicates whether the conditions are favor
 1. Color code the button to be different colors based on the weather conditions
 
 WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+THEN I am presented with a 5 - day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 1. 5 bootstrap cards that are made and display API information
 
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
 1. When we pull localStorage we are going to create buttons or links that then search that city.
-    Our links will run the same function that our initial search runs just with a different city name.*/
+Our links will run the same function that our initial search runs just with a different city name.*/
